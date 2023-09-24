5f3bb49fde8dd8fb14f14f8a31a7be35e3386dc4
kaushik-2318 <kaushikverma321@gmail.com>
2023-09-25 01:14:02 +0530

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,24 @@
 const connectToMongo = require('./db');
 const express = require('express')
 connectToMongo();
-
+9
 const app = express()
 const port = 3000
 
+//Availabe Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 app.get('/', (req, res) => {
   res.send('Hello Kaushik!')
 })
+
+// app.get('/api/v1/login', (req, res) => {
+//     res.send('Hello login!')
+// })
+
+// app.get('/api/v1/signup', (req, res) => {
+//     res.send('Hello signup!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
