dcbfbcadb6df800f0cb5f733f0a654b074a3b767
kaushik-2318 <kaushikverma321@gmail.com>
2023-09-26 16:31:37 +0530

    49 completed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Hello';
 
 //Create a User using: post"/api/auth/Create User". Doesn't require auth
 
@@ -18,20 +22,35 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //Check whether the user with this email already
     try {
+      //Check whether the user with this email already
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res
           .status(400)
           .json({ error: "This account is already registered" });
       }
+
+      var salt = await bcrypt.genSaltSync(10);
+      var secPass = await bcrypt.hash(req.body.password, salt)
+
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
-      res.json(user);
+
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      console.log(authtoken);
+      // res.json(user);
+      res.json({authtoken})
+
+
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occur");
